da713fca8a15d2102d94b97fda83b71e9bd5b073
nisikin <nisikin_1125@163.com>
2025-12-22 12:39:20 +0800

    1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/prometheus/:path*',
+        destination: 'http://10005480di8ni.vicp.fun/api/v1/:path*', // 代理到组长的穿透地址
+      },
+    ];
+  },
 };
 
 export default nextConfig;
