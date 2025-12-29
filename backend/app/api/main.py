c420c5599df46332675b464212288e621756c301
卢钰铠 <luyukai@luyukaideMBP-2.bbrouter>
2025-12-29 22:42:48 +0800

    add mcp service to generate PromQL

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 from api.route import health
 from api.route import metric
+from api.route import llm
 
 
 
 api_router = APIRouter()
 api_router.include_router(health.router)
 api_router.include_router(metric.router)
+api_router.include_router(llm.router)
