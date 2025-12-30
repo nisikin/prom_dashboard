80b5dd57e40e7c556cd22bcf44ba7e34abe4856f
ChloeWeever <luyukai47@outlook.com>
2025-12-30 19:23:55 +0800

    add custom query

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from api.route import health
 from api.route import metric
 from api.route import llm
+from api.route import custom
 
 
 
@@ -9,3 +10,4 @@ api_router = APIRouter()
 api_router.include_router(health.router)
 api_router.include_router(metric.router)
 api_router.include_router(llm.router)
+api_router.include_router(custom.router)
